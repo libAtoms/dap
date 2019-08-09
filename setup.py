bafca5c61b2dc443df006edf082b3747b3af6a51
Noam Bernstein <noam.bernstein@nrl.navy.mil>
2019-08-09 17:51:04 -0400

    Update davtkrc to new positional args, and rename to daprc

--- setup.py
+++ setup.py
@@ -9,11 +9,11 @@ setup(name='dap',
       author='Noam Bernstein',
       author_email='noam.bernstein@nrl.navy.mil',
       packages=find_packages(),
-      data_files=[('config',['davtkrc'])],
+      data_files=[('config',['daprc'])],
       scripts=['dap'],
       install_requires=['ase','vtk']
      )
 
 print("")
 print("Don't forget to")
-print("     cp davtkrc ~/.davtkrc")
+print("     cp daprc ~/.daprc")
