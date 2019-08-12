047256b410208e6cc95e12a8543368608d921222
Noam Bernstein <noam.bernstein@nrl.navy.mil>
2019-08-12 10:03:48 -0400

    Add README, optional format for dap atoms files, correctly handle filename with @slice

--- setup.py
+++ setup.py
@@ -9,11 +9,12 @@ setup(name='dap',
       author='Noam Bernstein',
       author_email='noam.bernstein@nrl.navy.mil',
       packages=find_packages(),
-      data_files=[('config',['daprc'])],
+      data_files=[('config',['daprc, README'])],
       scripts=['dap'],
       install_requires=['ase','vtk']
      )
 
 print("")
-print("Don't forget to check out daprc (in this directory) for example settings, and/or copy it:")
+print("Don't forget to check out README")
+print("See daprc (in this directory) for example settings, and/or copy it:")
 print("     cp "+os.path.join(os.environ["PWD"],"daprc")+" ~/.daprc")
