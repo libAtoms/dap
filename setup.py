75652247c8d85e4112e2aa981ec87eed3f9a0272
Noam Bernstein <noam.bernstein@nrl.navy.mil>
2019-09-13 13:45:53 -0400

    setup.py save Makefile for easier installation

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(name='dap',
       author='Noam Bernstein',
       author_email='noam.bernstein@nrl.navy.mil',
       packages=find_packages(),
-      data_files=[('config',['daprc', 'README'] + glob.glob('example.*'))],
+      data_files=[('config',['daprc', 'README', 'Makefile'] + glob.glob('example.*'))],
       scripts=['dap'],
       install_requires=['ase','vtk','ffmpeg-python']
      )
@@ -17,6 +17,7 @@ setup(name='dap',
 print("")
 print("Don't forget to check out README in this directory")
 print("See daprc for some example settings, and make your own in ~/.daprc")
+print("")
 print("More examples at example.* in this directory, view with")
 print('     dap -e "read example.settings" -e "read example.commands" example.xyz')
 
