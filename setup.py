cbcc9afe7b377db814b4c33c806c59bedae535ab
Noam Bernstein <noam.bernstein@nrl.navy.mil>
2020-03-20 12:58:09 -0400

    Incremenet version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 import os, glob
 
 setup(name='dap',
-      version='0.8.16',
+      version='0.8.17',
       description='Display atomic configuration',
       author='Noam Bernstein',
       author_email='noam.bernstein@nrl.navy.mil',
